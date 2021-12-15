0fcc0cd98d05240a69d1900fd8211bd51c3e17ef
Kian <kezaz@ucsd.edu>
2021-12-15 12:51:09 -0800

    Update user with added item

--- server/routes/pantryRouter.js
+++ server/routes/pantryRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 let Items = require('../models/items');
+let User = require('../models/user');
 
 router.route("/").get((req, res) => {
     Items.find()
@@ -11,11 +12,35 @@ router.route("/add").post((req, res) => {
     const newItem = new Items({
         name: req.body.name,
         count: req.body.count,
-        expirationDate: req.body.expirationDate
+        expirationDate: req.body.expirationDate,
+        user: req.body.user
     });
     newItem.save()
-        .then(() => res.json(newItem))
-        .catch(err => res.status(400).json("Error:" + err));
+        .then((err, savedItem) => {
+            if (err) {
+                res.status(500).send({ message: err });
+            }
+            User.findOne({ id: req.body.user })
+                .exec((err, user) => {
+                    if (err) {
+                        res.status(500).send({ message: err });
+                        return;
+                    }
+
+                    user.recipes.push(savedItem._id);
+                    user.save((err, updatedUser) => {
+                        if (err) {
+                            res.status(500).send({ message: err });
+                            return;
+                        }
+
+                        res.status(200).send({
+                            itemAdded: savedItem,
+                            updatedUser: updatedUser
+                        });
+                    });
+                });
+        })
 });
 
 router.route("/searchItem").get((req, res) => {
