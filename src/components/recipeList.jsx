7719ed01f550a28aca38bbe7f518e86804899fbc
Kian <kezaz@ucsd.edu>
2021-12-16 13:59:04 -0800

    check if unmounted

--- src/components/recipeList.jsx
+++ src/components/recipeList.jsx
@@ -7,34 +7,41 @@ const RecipeList = (props) => {
     const [recipeList, setRecipeList] = useState([]);
 
     useEffect(() => {
+        let mounted = true;
         let ingredients = props.items.map(item => item.name).join(',+');
-        axios.get('http://localhost:5000/recipes/apiKey')
+        if (true) {
+            axios.get('http://localhost:5000/recipes/apiKey')
             .then(res => {
                 let url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${res.data}&ingredients=${ingredients}`;
                 axios.get(url)
                     .then(recipes => {
                         console.log(recipes);
-                        setRecipeList(recipes.data.map(recipe => {
-                            return ({
-                                id: recipe.id,
-                                name: recipe.title,
-                                presentIngredients: recipe.usedIngredients.map(ingredient => {
-                                    return {
-                                        name: ingredient.name,
-                                        id: ingredient.id
-                                    }
-                                }),
-                                missingIngredients: recipe.missedIngredients.map(ingredient => {
-                                    return {
-                                        name: ingredient.name,
-                                        id: ingredient.id
-                                    }
-                                }),
-                                image: recipe.image
-                            });
-                        }));
+                        if (mounted) {
+                            setRecipeList(recipes.data.map(recipe => {
+                                return ({
+                                    id: recipe.id,
+                                    name: recipe.title,
+                                    presentIngredients: recipe.usedIngredients.map(ingredient => {
+                                        return {
+                                            name: ingredient.name,
+                                            id: ingredient.id
+                                        }
+                                    }),
+                                    missingIngredients: recipe.missedIngredients.map(ingredient => {
+                                        return {
+                                            name: ingredient.name,
+                                            id: ingredient.id
+                                        }
+                                    }),
+                                    image: recipe.image
+                                });
+                            }));
+                        }
                     });
-            });
+            })
+        }
+        console.log("here");
+        return () => { mounted = false };
     }, []);
 
     function getRecipeList() {
