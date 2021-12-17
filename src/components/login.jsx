db2b2796a09d27a944da14b2f23adedaa4281691
Kian <kezaz@ucsd.edu>
2021-12-16 16:19:25 -0800

    login styling

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,7 @@
+import './login.css';
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import background from '../grocery-produce.jpg';
 
 import AuthService from '../services/authService';
 
@@ -39,44 +41,64 @@ const Login = (props) => {
         });
     }
 
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label>Username: </label>
-                    <input 
-                        required 
-                        type="text" 
-                        className="form-control"
-                        value={username}
-                        onChange={onChangeUsername}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Password: </label>
-                    <input 
-                        required 
-                        type="password" 
-                        className="form-control"
-                        value={password}
-                        onChange={onChangePassword}
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                        type="submit"
-                        className="btn btn-primary"
-                    />
-                </div>
-            </form>
+    function onRegister(e) {
+        e.preventDefault()
+        window.location="/signup"
+    }
 
-            {message && (
-                <div className="form-group">
-                    <div className="alert alert-danger" role="alert">
-                        {message}
+    return (
+        <div style={{ 
+            backgroundImage: `url(${background})`,
+            height: "100vh",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+          }}>
+
+            <div className="form-container">
+                <form onSubmit={onSubmit}>
+                    <div className="inputs-container">
+                        <div className="form-group">
+                            <label>Username: </label>
+                            <input 
+                                required 
+                                type="text" 
+                                className="form-control"
+                                value={username}
+                                onChange={onChangeUsername}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label id="password">Password: </label>
+                            <input 
+                                required 
+                                type="password" 
+                                className="form-control"
+                                value={password}
+                                onChange={onChangePassword}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                                id="submit-button" 
+                                type="submit"
+                                className="btn btn-primary"
+                                value="Log in"
+                            />
+                        </div>
+                        <button id="register-button" onClick={onRegister}>New user? Sign up</button>
                     </div>
-                </div>
-          )}
+                    
+                </form>
+
+                {message && (
+                    <div className="form-group">
+                        <div className="alert alert-danger" role="alert">
+                            {message}
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
